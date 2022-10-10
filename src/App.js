706851764276968251a77515d2a5df57bc866c99
yewtree8 <mattygallimo@googlemail.com>
2022-10-10 19:02:44 +0100

    feat(master) start reducer store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Profile from './components/Profile';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+
+      <Profile />
+
+      <Login />
+
     </div>
   );
 }
